25f40694be2decabd19884732dfe42686cfd75cd
VI-Alumno <VI-Alumno@duoc.acad>
2026-06-17 09:15:50 -0400

    hay que probar swagger y hateoas

---
+++ src/main/java/dsy1103/bibliotecaam/taller/assembler/TallerModelAssembler.java
@@ -0,0 +1,18 @@
+package dsy1103.bibliotecaam.taller.assembler;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import dsy1103.bibliotecaam.taller.controller.TallerController;
+import dsy1103.bibliotecaam.taller.dto.TallerResponseDTO;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TallerModelAssembler implements RepresentationModelAssembler<TallerResponseDTO, EntityModel<TallerResponseDTO>> {
+    @Override
+    public EntityModel<TallerResponseDTO> toModel(TallerResponseDTO tallerDto){
+        return EntityModel.of(tallerDto,
+                linkTo(methodOn(TallerController.class).obtenerPorId(tallerDto.getIdTaller())).withSelfRel(),
+                linkTo(methodOn(TallerController.class).obtenerTodas()).withRel("talleres"));
+    }
+}
--- src/main/java/dsy1103/bibliotecaam/taller/controller/TallerController.java
+++ src/main/java/dsy1103/bibliotecaam/taller/controller/TallerController.java
@@ -1,10 +1,24 @@
 package dsy1103.bibliotecaam.taller.controller;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+import dsy1103.bibliotecaam.taller.assembler.TallerModelAssembler;
 import dsy1103.bibliotecaam.taller.dto.TallerRequestDTO;
 import dsy1103.bibliotecaam.taller.dto.TallerResponseDTO;
+import dsy1103.bibliotecaam.taller.model.Taller;
 import dsy1103.bibliotecaam.taller.service.TallerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,39 +27,75 @@ import java.time.LocalDate;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/bibliotecaam/taller")
 @RequiredArgsConstructor
+@Tag(name = "Talleres", description = "Lista de talleres de la biblioteca")
 public class TallerController {
     private final TallerService tallerService;
+    @Autowired
+    private TallerModelAssembler assembler;
 
-    @GetMapping
-    public ResponseEntity<List<TallerResponseDTO>> obtenerTodas() {
-        return ResponseEntity.ok(tallerService.obtenerTodas());
+    @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    @ArraySchema(schema = @Schema(implementation = Taller.class))
+    @Operation(summary = "Obtener los datos de todos los talleres.", description = "Esta opcion retornara los datos de todos los talleres en la Base de Datos.")
+    public ResponseEntity<CollectionModel<EntityModel<TallerResponseDTO>>> obtenerTodos(){
+        List<EntityModel<TallerResponseDTO>> talleres = tallerService.obtenerTodas().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(CollectionModel.of(talleres,
+                linkTo(methodOn(TallerController.class).obtenerTodos()).withSelfRel()));
     }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<TallerResponseDTO> obtenerPorId(@PathVariable Long id) {
+    @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    @Operation(summary = "Obtener los datos por el id del taller", description = "Se retornara el taller que coincida con el id ingresado.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "¡Taller encontrado con exito!"),
+            @ApiResponse(responseCode = "404",description = "ERROR: ¡El id del taller ingresado no existe!")
+    })
+    public ResponseEntity<EntityModel<TallerResponseDTO>> obtenerPorId(@PathVariable Long id){
         return tallerService.obtenerPorId(id)
+                .map(assembler::toModel)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/nombre/{nombre}")
-    public ResponseEntity<List<TallerResponseDTO>> obtenerTallerPorNombre(@PathVariable String nombre) {
-        if (tallerService.obtenerPorNombre(nombre).isEmpty()){
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.ok(tallerService.obtenerPorNombre(nombre));
+    @ArraySchema(schema = @Schema(implementation = Taller.class))
+    @Operation(summary = "Obtener los talleres por el nombre", description = "Se retornaran los talleres que coincidan con el nombre ingresado.")
+    public ResponseEntity<CollectionModel<EntityModel<TallerResponseDTO>>> obtenerPorISBN(@PathVariable String nombre){
+        List<EntityModel<TallerResponseDTO>> talleres = tallerService.obtenerPorNombre(nombre).stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(CollectionModel.of(talleres,
+                linkTo(methodOn(TallerController.class).obtenerPorISBN(nombre)).withSelfRel()));
     }
 
     @PostMapping
-    private ResponseEntity<TallerResponseDTO> guardar(@Valid @RequestBody TallerRequestDTO dto, @RequestHeader("Authorization") String token){
-        return ResponseEntity.status(201).body(tallerService.guardar(dto, token));
+    @Operation(summary = "Crear un nuevo taller en la Base de Datos", description = "Se creara y guardaran los datos de un nuevo taller creado en la Base de Datos.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "¡Taller actualizado con exito!",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Taller.class))),
+            @ApiResponse(responseCode = "400",description = "Puede que no se este comunicando con la tabla Donacion/Faltan parametros.")
+    })
+    private ResponseEntity<EntityModel<TallerResponseDTO>> guardar(@Valid @RequestBody TallerRequestDTO dto, @RequestHeader("Authorization") String token){
+        TallerResponseDTO nuevoTaller = tallerService.guardar(dto, token);
+        return ResponseEntity.status(201).body(assembler.toModel(nuevoTaller));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar un taller de la Base de Datos", description = "Se actualizaran los datos de un taller ingresando su id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "¡Taller actualizado con exito!",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Taller.class))),
+            @ApiResponse(responseCode = "404",description = "ERROR: ¡El id del taller ingresado no existe!"),
+            @ApiResponse(responseCode = "400",description = "Puede que no se este comunicando con la tabla taller/Faltan parametros.")
+    })
     public ResponseEntity<TallerResponseDTO> actualizar(
             @PathVariable Long id,
             @Valid @RequestBody TallerRequestDTO dto,
