58c2c9069466734c9447d00e27281cd02f611905
MAGAQ <panamasado41@gmail.com>
2026-06-23 05:18:47 -0400

    Añadi el boton y deje la anotacion para encender los candados. MS listo para el mambo >:D

--- src/main/java/dsy1103/bibliotecaam/taller/controller/TallerController.java
+++ src/main/java/dsy1103/bibliotecaam/taller/controller/TallerController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -29,6 +30,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/api/bibliotecaam/taller")
 @RequiredArgsConstructor
